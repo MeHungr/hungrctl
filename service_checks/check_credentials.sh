b1bdfe86d2269e451b8bcd8b02fc6f2e37c3a417
Dylan Weber <dylanweber06@gmail.com>
2025-04-09 03:10:10 -0400

    Fixed

--- service_checks/check_credentials.sh
+++ service_checks/check_credentials.sh
@@ -166,7 +166,8 @@ check_modified_users() {
                 event_log "SHELL-CHANGE" "Shell change for $user: $b_shell -> $c_shell"
                 echo "[$HOST] Shell change for $user: $b_shell -> $c_shell at $(timestamp)" >> "$SUMMARY_LOG"
             fi
-        done
+        fi
+    done
     if [ "$modified_found" = false ]; then
         log_ok "No modified users found."
     fi
